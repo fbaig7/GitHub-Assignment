c002c470290a795aba7eb3fbe6c1cf78992a82df
fbaig7 <166451264+fbaig7@users.noreply.github.com>
2024-04-08 23:09:40 -0700

    Add files via upload

---
+++ sortable-array.java
@@ -0,0 +1,22 @@
+package com.fatima;
+
+public class SortableArray {
+    public static void main(String[] args) {
+        int[] intArray = {5, 2, 8, 1, 6};
+        String[] stringArray = {"banana", "apple", "orange", "grapes"};
+
+        SortIntegerArray.sort(intArray);
+        
+        SortStringArray.sort(stringArray);
+
+        System.out.println("Sorted Integer Array:");
+        for (int num : intArray) {
+            System.out.print(num + " ");
+        }
+        
+        System.out.println("\nSorted String Array:");
+        for (String str : stringArray) {
+            System.out.print(str + " ");
+        }
+    }
+}
